78a8792b48f8ca062664dae18311f64fac106e7c
Carlo Cardona <carlo.dev2@gmail.com>
2020-10-22 20:26:28 +1100

    added landing pages for routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/pages/Navbar.js';
+import Home from './components/pages/Home';
+import News from './components/pages/News';
+import GetInvolved from './components/pages/GetInvolved';
+import SignIn from './components/pages/SignIn';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <Router>
-      <Navbar></Navbar>
+      <Navbar />
       <Switch>
-        <Route path='/' />
+        <Route path='/' exact component={Home} />
+        <Route path='/news' exact component={News} />
+        <Route path='/getinvolved' exact component={GetInvolved} />
+        <Route path='/signin' exact component={SignIn} />
+        <Route path='/signup' exact component={SignUp} />
       </Switch>
     </Router>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,24 +1,16 @@
 import React from 'react'
-import { Button } from './Button'
-import { Link } from 'react-router-dom'
 import './Home.css'
 
-function Home({ lightBg, topLine, lightText, lightTextDesc, headline, description, buttonLabel, img, alt, imgStart }) {
+function Home() {
     return (
         <>
-            <div className={lightBg ? 'home__home-section' : 'home__home-section darkBg'}>
+            <div className='row'>
                 <div className='container'>
                     <div className='row home__home-row'>
-                        <div className='top-line'>{topLine}</div>
-                        <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
-                        <p className={lightTextDesc ? 'home__home-subtitle' : 'home__home-subtitle dark'}>{description}</p>
-
+                        <div>Column 1</div>
+                        <div>Column 2</div>
                     </div>
-                </div>
-                <div className='col'>
-                    <div className='home__home-img-wrapper'>
-                        <img src={img} alt={alt} className='home__home-img' />
-                    </div>
+                    <div>Row 2</div>
                 </div>
             </div>
         </>
